faed78704b38112867fce9d1d89af4442681cff1
German <gerkamano@gmail.com>
2021-03-01 22:55:28 +0300

    added wallpaper,car,game mechanics

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from tkinter import *
+from PIL import ImageTk
+from PIL import Image as Img
+from random import shuffle  # Модуль рандом
 
 
-# Press the green button in the gutter to run the script.
+class Game:
+    # Инициализация класса.
+    def __init__(self):
+        self.game = Tk()  # Создание окна
+        self.game.title('Shashki')  # Название программы
+        # self.ma1n.iconbitmap('favicon.ico')  # Добавляем иконку
+        self.game.geometry('330x660')  # Определяем размер окна
+        self.game.resizable(False, False)  # Окно нельзя расширять по осям Х и У
+        self.game.attributes('-topmost', True)  # Окно при запуске уходит на передний план
+        self.game.bind('<KeyRelease-a>', lambda event: self.left())
+        self.game.bind('<KeyRelease-d>', lambda event: self.right())
+        self.car_position = 1
+        self.background_image = ImageTk.PhotoImage(Img.open('wallpaper.png'))
+        self.background_label = Label(self.game, image=self.background_image)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.car_image = ImageTk.PhotoImage(Img.open('car.png'))
+        self.car_label = Label(self.game, image=self.car_image, border='0', relief=FLAT)
+        self.car_label.place(x=115, y=550)
+
+        self.game.mainloop()
+
+    def left(self):
+        if self.car_position == 0:
+            pass
+        elif self.car_position == 1:
+            self.car_label.place(x=10, y=550)
+            self.car_position = 0
+        else:
+            self.car_label.place(x=115, y=550)
+            self.car_position = 1
+
+    def right(self):
+        if self.car_position == 0:
+            self.car_label.place(x=115, y=550)
+            self.car_position = 1
+        elif self.car_position == 1:
+            self.car_label.place(x=220, y=550)
+            self.car_position = 2
+        else:
+            pass
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    _ = Game()
