3ea48ef4aaf2e701aacd45495ff67c060dd8c813
German <gerkamano@gmail.com>
2021-03-02 01:55:36 +0300

    major fixes: + added collision + added gameplay + added random spawn + added lose button + added scores

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from random import randint
 class Game:
     # Инициализация класса.
     def __init__(self):
+        self.width, self.height = 460, 660
         self.game = Tk()  # Создание окна
         self.game.title('Shashki')  # Название программы
         # self.ma1n.iconbitmap('favicon.ico')  # Добавляем иконку
-        self.game.geometry('460x660')  # Определяем размер окна
+        self.game.geometry(f'{self.width}x{self.height}')  # Определяем размер окна
         self.game.resizable(False, False)  # Окно нельзя расширять по осям Х и У
         self.game.attributes('-topmost', True)  # Окно при запуске уходит на передний план
         self.game.bind('<KeyRelease-Left>', lambda event: self.left())
@@ -19,11 +20,16 @@ class Game:
         self.game.bind('<KeyRelease-Right>', lambda event: self.right())
         self.game.bind('<KeyRelease-d>', lambda event: self.right())
         self.game.bind('<KeyRelease-D>', lambda event: self.right())
-        self.game.bind('<Return>', lambda event: self.spawn())
+
+        self.status = True
         self.player_position = 1
+        self.speed = 5
         self.car_position = 0
+        self.car_y = 0
         self.hole_position = 0
+        self.hole_y = 0
         self.score = 0
+        self.y = 0
 
         self.background_image = ImageTk.PhotoImage(Img.open('wallpaper.png'))
         self.background = Label(self.game, image=self.background_image)
@@ -35,29 +41,68 @@ class Game:
         self.hole = Label(self.game, image=self.hole_image, border='0', relief=FLAT)
         self.score_label = Label(self.game, text=self.score, font=('Arial', 20))
 
+        self.lose_label = Label(self.game, text=f'Вы проиграли!\nВаши очки: {self.score}.')
+        self.lose_button = Button(self.game, text='Повторить', command=self.start)
+
+        self.start()
+        self.game.mainloop()
+
+    def start(self):
+        self.lose_label.place_forget()
+        self.lose_button.place_forget()
+        self.status = True
+        self.player_position = 1
+        self.car_y = 0
+        self.hole_y = 0
+        self.score = 0
+        self.y = 0
         self.background.place(x=0, y=0, relwidth=1, relheight=1)
         self.player.place(x=130, y=550)
         self.score_label.place(x=360, y=50, width=50)
+        self.spawn('car')
+        self.spawn('hole')
+        self.loop()
 
-        self.spawn()
-        self.game.mainloop()
 
-    def spawn(self):
-        self.hole_position = randint(0, 3)
-        if self.car_position == 0:
-            self.hole.place(x=25, y=20)
-        elif self.hole_position == 1:
-            self.hole.place(x=130, y=20)
-        else:
-            self.hole.place(x=235, y=20)
+    def lose(self):
+        self.lose_label.place(x=0, y=0)
+        self.lose_button.place(x=50, y=50)
+        self.status = False
 
-        self.car_position = randint(0, 3)
-        if self.car_position == 0:
-            self.car.place(x=25, y=120)
-        elif self.car_position == 1:
-            self.car.place(x=130, y=120)
-        else:
-            self.car.place(x=235, y=120)
+    def spawn(self, obstacle):
+        self.y = 0
+        if obstacle == 'hole':
+            self.hole_y = randint(0, 100)+10*self.y
+            self.hole_position = randint(0, 2)
+            self.hole.place(x=25+105*self.hole_position, y=self.hole_y)
+        if obstacle == 'car':
+            self.car_y = randint(0, 100)+10*self.y
+            while self.car_position != self.hole_position:
+                self.car_position = randint(0, 2)
+            self.car.place(x=25+105*self.car_position, y=self.car_y)
+
+    def loop(self):
+        if self.status:
+            self.y += 1
+            self.hole_y += self.speed
+            self.car_y += self.speed
+            self.hole.place_forget()
+            self.hole.place(x=25+105*self.hole_position, y=self.hole_y)
+            self.car.place_forget()
+            self.car.place(x=25+105*self.car_position, y=self.car_y)
+            if self.hole_position == self.player_position and 647 >= self.hole_y >= 454:
+                self.lose()
+            if self.car_position == self.player_position and 647 >= self.car_y >= 454:
+                self.lose()
+            if self.hole_y >= self.height:
+                self.spawn('hole')
+                self.score += 1
+            if self.car_y >= self.height:
+                self.spawn('car')
+                self.score += 1
+            self.score_label['text'] = self.score
+
+            self.game.after(150, self.loop)
 
     def left(self):
         if self.player_position == 0:
