04d28fd44d433b05adf7f59c1141ee505704e1be
German <gerkamano@gmail.com>
2021-03-01 23:17:08 +0300

    added more buttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from PIL import ImageTk
 from PIL import Image as Img
-from random import shuffle  # Модуль рандом
+from random import randint
 
 
 class Game:
@@ -10,19 +10,27 @@ class Game:
         self.game = Tk()  # Создание окна
         self.game.title('Shashki')  # Название программы
         # self.ma1n.iconbitmap('favicon.ico')  # Добавляем иконку
-        self.game.geometry('330x660')  # Определяем размер окна
+        self.game.geometry('460x660')  # Определяем размер окна
         self.game.resizable(False, False)  # Окно нельзя расширять по осям Х и У
         self.game.attributes('-topmost', True)  # Окно при запуске уходит на передний план
+        self.game.bind('<KeyRelease-Left>', lambda event: self.left())
         self.game.bind('<KeyRelease-a>', lambda event: self.left())
+        self.game.bind('<KeyRelease-A>', lambda event: self.left())
+        self.game.bind('<KeyRelease-Right>', lambda event: self.right())
         self.game.bind('<KeyRelease-d>', lambda event: self.right())
+        self.game.bind('<KeyRelease-D>', lambda event: self.right())
         self.car_position = 1
+        self.score = 0
+
         self.background_image = ImageTk.PhotoImage(Img.open('wallpaper.png'))
         self.background_label = Label(self.game, image=self.background_image)
-        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
         self.car_image = ImageTk.PhotoImage(Img.open('car.png'))
         self.car_label = Label(self.game, image=self.car_image, border='0', relief=FLAT)
-        self.car_label.place(x=115, y=550)
+        self.score_label = Label(self.game, text=self.score, font=('Arial', 20))
+
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.car_label.place(x=130, y=550)
+        self.score_label.place(x=360, y=50, width=50)
 
         self.game.mainloop()
 
@@ -30,18 +38,19 @@ class Game:
         if self.car_position == 0:
             pass
         elif self.car_position == 1:
-            self.car_label.place(x=10, y=550)
+            self.car_label.place(x=25, y=550)
             self.car_position = 0
+
         else:
-            self.car_label.place(x=115, y=550)
+            self.car_label.place(x=130, y=550)
             self.car_position = 1
 
     def right(self):
         if self.car_position == 0:
-            self.car_label.place(x=115, y=550)
+            self.car_label.place(x=130, y=550)
             self.car_position = 1
         elif self.car_position == 1:
-            self.car_label.place(x=220, y=550)
+            self.car_label.place(x=235, y=550)
             self.car_position = 2
         else:
             pass
